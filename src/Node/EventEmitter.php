bd0d847d60e822fe29e6e038d484be93f6e8e9c0
0x1 <kevin.francart3@gmail.com>
2026-07-14 01:41:17 +0200

    Fix unsafeOn to support closures

--- src/Node/EventEmitter.php
+++ src/Node/EventEmitter.php
@@ -5,6 +5,9 @@ $exports = [];
 $exports['unsafeOn'] = function($emitter, $event, $listener) {
     if (method_exists($emitter, 'on')) {
         $emitter->on($event, $listener);
+    } elseif (isset($emitter->on) && is_callable($emitter->on)) {
+        $f = $emitter->on;
+        $f($event, $listener);
     }
 };
 
